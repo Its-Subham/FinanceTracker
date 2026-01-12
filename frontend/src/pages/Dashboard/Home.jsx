29cb235c66bc3c832013d3077e2efb5aba913913
Its-Subham <subhambera80062@gmail.com>
2026-01-12 16:43:33 +0530

    Impliment Dashboard UI

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import DashboardLayout from '../../components/layout/DashboardLayout'
-import { useUserAuth } from '../../hooks/useUserAuth'
-import { useNavigate } from 'react-router-dom'
-import axiosInstance from '../../utils/axiosInstance'
-import { API_PATHS } from '../../utils/apiPaths'
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../../components/layout/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import InfoCard from "../../components/Cards/InfoCard";
+import { addThousandsSeparator } from "../../utils/helper";
+import RecentTransactions from "../../components/layout/RecentTransactions";
+
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { IoMdCard } from "react-icons/io";
 
 const Home = () => {
   useUserAuth();
@@ -12,21 +18,25 @@ const Home = () => {
 
   const [dashboardData, setDashboardData] = useState(null);
   const [loading, setLoading] = useState(false);
-  
+
   const fetchDashboardData = async () => {
     if (loading) return;
     setLoading(true);
     try {
-      const response = await axiosInstance.get(`${API_PATHS.DASHBOARD.GET_DATA}`);
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
       if (response.data) {
         setDashboardData(response.data);
+        console.log("Transactions", response.data);
+        // console.log("Dashboard Data", dashboardData);
       }
     } catch (error) {
       console.log("Something went wrong. Please try again.", error);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     fetchDashboardData();
@@ -35,11 +45,39 @@ const Home = () => {
 
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className='my-5 mx-auto'>
-        Home
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            label="Total Income"
+            value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500"
+          />
+
+          <InfoCard
+            icon={<LuHandCoins />}
+            label="Total Expenses"
+            value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+            color="bg-red-500"
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          <RecentTransactions
+            transactions={dashboardData?.recentTransactions}
+            onSeeMore={() => navigate("/expense")}
+          />
+        </div>
       </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
